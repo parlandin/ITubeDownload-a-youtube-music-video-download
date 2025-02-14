95560fef033365a4215d166194b57b81173ac7cb
parlandin <56051040+parlandin@users.noreply.github.com>
2025-02-14 18:37:38 -0300

    fix: switch ffmpeg installer to ffmpeg-static and add logging for development

--- src/main/utils/ffmpeg.ts
+++ src/main/utils/ffmpeg.ts
@@ -1,13 +1,17 @@
-import fmpegPath from "@ffmpeg-installer/ffmpeg";
+import fmpegPath from "ffmpeg-static";
 import { is } from "@electron-toolkit/utils";
 
 const paths = {
-  ffmpeg: fmpegPath.path
+  ffmpeg: fmpegPath || ""
 };
 
 if (!is.dev) {
   const fixPath = (path: string): string => path.replace("app.asar", "app.asar.unpacked");
   paths.ffmpeg = fixPath(paths.ffmpeg);
+}
+
+if (is.dev) {
+  console.log("ffmpeg path: ", paths.ffmpeg);
 }
 
 //ffmpeg.setFfmpegPath(paths.ffmpeg);
